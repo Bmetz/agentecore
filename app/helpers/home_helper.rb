8b19c54fdb8dfc78d55b5f65288cfeef9c62fcef
Joao Paulo Barros <jpsb@jpsbpc2.credishop.com.br>
2009-07-31 19:05:57 -0300

    Ajustes No Layout

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,21 +1,22 @@
 module HomeHelper
-  
-  def newest_pictures limit = 12
+
+  def newest_pictures limit = 4
     Photo.find(:all, :order => 'created_at desc', :limit => limit)
   end
-  
-  
-  def recent_comments limit = 10
+
+
+  def recent_comments limit = 5
     Comment.find(:all, :order => 'created_at desc', :limit => limit, :conditions => "commentable_type='Profile'")
   end
-  
-  def new_members(limit = 12)
+
+  def new_members(limit = 5)
     Profile.find(:all, :limit => limit, :order => 'created_at DESC', :conditions=>"user_id is not null")
   end
-  
-  def recent_forum_posts(limit = 10)
+
+  def recent_forum_posts(limit = 5)
     ForumTopic.find(:all, :limit => limit, :order => 'forum_posts.created_at DESC', :include => [:forum, :posts])
   end
-  
-  
+
+
 end
+
